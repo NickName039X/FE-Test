3b66b5a6fdbaaef91d9f52ca1c61b25975a543a4
whxmm0608 <1159460622@qq.com>
2019-09-17 13:25:38 +0800

    ~

---
+++ js/test2.js
@@ -0,0 +1,9 @@
+const map = new Map ();
+var obj = {a:1};
+map.set ('name', 'ft');
+map.set ('phone',1383838438);
+map.set (obj, 'haha');
+map.get ('name');
+
+// map取对象时，比较的是引用地址，所以要 传一引用地址作为key去查询
+map.get (obj);
